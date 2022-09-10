f3dd8d4f69f4e9b3de0fb0627a337a788cce320c
ibktommy <atomquartz@gmail.com>
2022-09-10 15:24:03 +0100

    Working on getting Data from Cloud-Firestore

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from './firebase'
 
 function App() {
   // App State 
   const [users, setUsers] = useState([])
+
+  // Getting Data-Collection from cloud-firestore
+  const usersCollectionRef = collection(db, "users")
+
+  // UseEffect Hook Functionality that runs after the component renders
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef)
+      console.log(data)
+    }
+
+    getUsers()
+  }, [usersCollectionRef])
 
   return (
     <h1>CRUD APP: REACT/FIREBASE</h1>
--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MAESSAGING_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
@@ -19,3 +19,5 @@ const app = initializeApp(firebaseConfig);
 
 // Export database object-value
 export const db = getFirestore(app)
+
+export default app
