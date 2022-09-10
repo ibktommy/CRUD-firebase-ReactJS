46a3f5c8a92f4bdc2556f40660e90f2a13a40291
ibktommy <atomquartz@gmail.com>
2022-09-10 12:26:51 +0100

    reconfiguring new firebase Project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import { collection } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { db } from './firebaseConfig'
+import React, { useState } from 'react'
 
 function App() {
   // App State 
   const [users, setUsers] = useState([])
-
-  // Function to Fetch Data
-  async function getUsers() {
-    const usersCollectionRef = collection(db, 'users')
-    console.log(usersCollectionRef)
-  }
-
-  // Fetching Data From FireStore in UseEffect Hook whenever the page renders
-  useEffect(() => {
-    getUsers()
-  }, [])
 
   return (
     <h1>CRUD APP: REACT/FIREBASE</h1>
--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_APP_ID,
-  appId: process.env.REACT_APP__FIREBASE_MEASUREMENT_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MAESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase FireStore with info from the app
-export const db = getFirestore(app)
-
+const db = getFirestore(app)
