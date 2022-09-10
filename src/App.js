9ea387781eabdad3c57aec5f666c60d6aa62d5df
ibktommy <atomquartz@gmail.com>
2022-09-10 11:34:03 +0100

    Create App State where we want to save data from the database, create function that fetches data from the cloud-firestore and call the function tn the useEffect Hook after the App component renders.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+import { collection } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from './firebaseConfig'
+
 function App() {
+  // App State 
+  const [users, setUsers] = useState([])
+
+  // Function to Fetch Data
+  async function getUsers() {
+    const usersCollectionRef = collection(db, 'users')
+    console.log(usersCollectionRef)
+  }
+
+  // Fetching Data From FireStore in UseEffect Hook whenever the page renders
+  useEffect(() => {
+    getUsers()
+  }, [])
+
   return (
     <h1>CRUD APP: REACT/FIREBASE</h1>
   );
